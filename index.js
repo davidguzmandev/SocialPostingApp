ad159fe8406e22d432e71646791b805cfea98ac4
davidruncode <daguz.91@gmail.com>
2024-05-26 23:08:15 -0400

    Edit, Update(error), Delte

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const urlPost = 'https://jsonplaceholder.typicode.com/posts/'
+const urlPost = 'https://jsonplaceholder.typicode.com/posts'
 let posts = [];
 
 function getPost(){
@@ -34,7 +34,84 @@ function renderPost() {
     })
 }
 
+function postData(){
+    const postBody = document.getElementById('post');
+    const postBodyInput = postBody.value;
+
+    if (postBodyInput.trim() == ''){
+        alert('Please write anything');
+        return;
+    }
+
+    fetch(urlPost, {
+        method: 'POST',
+        body: JSON.stringify({
+          body: postBodyInput,
+          userId: 1,
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
+    .then(res => res.json())
+    .then(data => {
+        posts.unshift(data);
+        renderPost()
+    }).catch(err => {
+        console.log(err);
+    })
+
+    postBody.value = ''
+}
+
 function editPost(id) {
     const form = document.getElementById(`editForm${id}`);
-    form.style.display = 'block';
+    form.style.display = (form.style.display == 'none') ? 'block' : 'none' ;
+}
+
+function updatePost(id) {
+    const editBody = document.getElementById(`editBody${id}`).value;
+    console.log(id);
+    console.log(posts);
+    
+    fetch(`${urlPost}/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            id: id,
+            body: editBody,
+            userId: 1,
+          }),
+          headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+          },
+    })
+    .then(res => res.json())
+    .then(data => {
+        const index = posts.findIndex(post => post.id === data.id)
+        if (index != -1) {
+            posts[index] = data;
+        } else {
+            alert('Something wrong');
+        }
+        renderPost();
+    }).catch(err => {
+        console.log(err);
+    })
+}
+
+function deletePost(id) {
+    fetch(`${urlPost}/${id}`, {
+        method: 'DELETE',
+    })
+    .then(res => {
+        if(res.ok) {
+            posts = posts.filter( post => post.id != id);
+            renderPost();
+        } else {
+            alert('Something wrong');
+        }
+    })
+    .catch(err => {
+        console.log(err);
+    })
 }
